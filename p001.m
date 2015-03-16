ba8084e152ef98fb4ed8e8bf5fded809f54edb83
tlenetto <p.nitipong@gmail.com>
2015-03-16 14:54:54 +0700

    edit plot process

--- p001.m
+++ p001.m
@@ -1,11 +1,15 @@
-%x = input('Enter x = ');
+x = input('Enter x = ');
 fprintf('x = %f\n',x);
 % edit online by user 1
 x = input('Enter Number = ');
 for i = 1:x
   fprintf('i = %d\n',i);
 end
-x = 1:1:100;
+x2 = 1:1:100;
 % edit by local 
-y = x.^2;
-plot(x,y);
+y2 = x2.^2;
+%plot(x,y);
+plot(x2,y2,'-ok');
+xlabel('x');
+ylabel('f(x)');
+title('Plot f(x)');
